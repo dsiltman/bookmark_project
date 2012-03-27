c8eac2a6d36dc148deb6f04e88edbaa4674a1afa
DeLaina <toymom@toymom.com>
2012-03-26 20:38:40 -0500

    Starting Project for second time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookmarkProject::Application.config.secret_token = '9c073ebd702df107ccf26a1676929c6ef31040337982fc63298d01e9117704d4f38f0b4555d8ff8887286b5633ce611fe8b89c8edf7327c1b629ac5fa8682378'
